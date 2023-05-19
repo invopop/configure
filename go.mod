d4a91c0d2240f4fa4019c34b290505d2ad40c876
Sam Lown <sam.lown@invopop.com>
2023-05-19 13:21:23 +0000

    Adding grpc service configs

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/nats-io/nats.go v1.25.0
 	github.com/rs/zerolog v1.29.1
 	github.com/stretchr/testify v1.8.2
+	google.golang.org/grpc v1.55.0
 )
 
 require (
@@ -20,7 +21,10 @@ require (
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/grpcconf/grpcconf.go
@@ -0,0 +1,42 @@
+package grpcconf
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+)
+
+// Service defines a generic base for dealing with connection details
+// to an internal gRPC service.
+type Service struct {
+	Host string `json:"host"`
+	Port string `json:"port"`
+
+	// Insecure is required by gRPC to say when there are no TLS connection
+	// details.
+	Insecure bool `json:"insecure"`
+
+	// PublicURL defines the base url to use when forwarding the user to
+	// public side of the service. Not all services required this.
+	PublicURL string `json:"public_url"`
+}
+
+// DialOptions provides an array of gRPC DialOptions based on the
+// defined service configuration.
+func (s *Service) DialOptions() []grpc.DialOption {
+	opts := []grpc.DialOption{}
+	if s.Insecure {
+		opts = append(opts, grpc.WithInsecure())
+	}
+	return opts
+}
+
+// URL provides the result of joining the Host and Port together.
+func (s *Service) URL() string {
+	return s.Host + ":" + s.Port
+}
+
+// Connection provides an instance of the grpc connection.
+func (s *Service) Connection(ctx context.Context) (*grpc.ClientConn, error) {
+	return grpc.DialContext(ctx, s.URL(), s.DialOptions()...)
+}
